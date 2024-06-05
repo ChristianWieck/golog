c061287ba587b8c6fdc00280a6eecbe92fd12a64
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-06-05 08:42:56 +0200

    Add format support for logging

--- log/log.go
+++ log/log.go
@@ -1,11 +1,12 @@
 package log
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 )
 
-func logInternal(level LogLevel, msg string) {
+func logInternal(level LogLevel, format string, a ...any) {
 	// Recover information about the caller
 	pc, file, no, ok := runtime.Caller(2) // We need to go two steps back
 	details := runtime.FuncForPC(pc)
@@ -14,32 +15,36 @@ func logInternal(level LogLevel, msg string) {
 		logDetails = CallerDetails{FileName: file, LineNumber: no, MethodName: details.Name()}
 	}
 
+	msg := ""
+	msgFormatted := false
 	for _, v := range logHandlers {
 		if level >= v.LogLevel {
+			if !msgFormatted {
+				msg = fmt.Sprintf(format, a...)
+				msgFormatted = true
+			}
 			v.Handle(level, msg, logDetails)
 		}
 	}
 }
 
-func Log(level LogLevel, msg string) {
-	logInternal(level, msg)
-}
+func Log(level LogLevel, format string, a ...any) { logInternal(level, format, a...) }
 
 // Log a message with "Debug" level
-func Debug(msg string) { logInternal(LOG_LEVEL_DEBUG, msg) }
+func Debug(format string, a ...any) { logInternal(LOG_LEVEL_DEBUG, format, a...) }
 
 // Log a message with "Info" level
-func Info(msg string) { logInternal(LOG_LEVEL_INFO, msg) }
+func Info(format string, a ...any) { logInternal(LOG_LEVEL_INFO, format, a...) }
 
 // Log a message with "Warning" level
-func Warning(msg string) { logInternal(LOG_LEVEL_WARNING, msg) }
+func Warning(format string, a ...any) { logInternal(LOG_LEVEL_WARNING, format, a...) }
 
 // Log a message with "Error" level
-func Error(msg string) { logInternal(LOG_LEVEL_ERROR, msg) }
+func Error(format string, a ...any) { logInternal(LOG_LEVEL_ERROR, format, a...) }
 
 // Log a message with "Critical" level. This will also interrupt the program once
 // the message is logged with an exit code of 1.
-func Critical(msg string) {
-	logInternal(LOG_LEVEL_CRITICAL, msg)
+func Critical(format string, a ...any) {
+	logInternal(LOG_LEVEL_CRITICAL, format, a...)
 	os.Exit(1)
 }
