88134df5e4cc59166b2d18e119635d2889bf0792
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-08-29 14:43:51 +0200

    Add milliseconds to console output

--- log/console_logger.go
+++ log/console_logger.go
@@ -9,7 +9,7 @@ type consoleLogger struct{}
 func (c consoleLogger) Handle(level LogLevel, msg string, details LogDetails) error {
 	logLevelToColor(level).Printf(
 		"%s - [%s] %s - %s\n",
-		time.Now().Local().Format("2006-01-02 15:04:05"),
+		time.Now().Local().Format("2006-01-02 15:04:05.000"),
 		logLevelToString(level),
 		details.LoggerName,
 		msg,
