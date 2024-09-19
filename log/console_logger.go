162bde4854604dae48b2f3330412cac1e571c15e
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-09-19 09:24:26 +0200

    Add String method to loglevel

--- log/console_logger.go
+++ log/console_logger.go
@@ -26,19 +26,3 @@ func RegisterConsoleLogger() {
 func RegisterConsoleLoggerVerbosity(level LogLevel) {
 	RegisterHandlerVerbosity(consoleLogger{}, level)
 }
-
-func logLevelToString(level LogLevel) string {
-	switch level {
-	case LogLevelDebug:
-		return "DEBUG"
-	case LogLevelInfo:
-		return "INFO"
-	case LogLevelWarning:
-		return "WARNING"
-	case LogLevelError:
-		return "ERROR"
-	case LogLevelCritical:
-		return "CRITICAL"
-	}
-	return ""
-}
--- log/log_level.go
+++ log/log_level.go
@@ -9,3 +9,23 @@ const (
 	LogLevelError
 	LogLevelCritical
 )
+
+func (level LogLevel) String() string {
+	return logLevelToString(level)
+}
+
+func logLevelToString(level LogLevel) string {
+	switch level {
+	case LogLevelDebug:
+		return "DEBUG"
+	case LogLevelInfo:
+		return "INFO"
+	case LogLevelWarning:
+		return "WARNING"
+	case LogLevelError:
+		return "ERROR"
+	case LogLevelCritical:
+		return "CRITICAL"
+	}
+	return ""
+}
